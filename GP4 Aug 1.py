a740343ae21a1be0d1d2b4ec2c2f5f5e3e5fe399
ka-lu <68350905+ka-lu@users.noreply.github.com>
2023-08-01 19:13:01 -0700

    Add files via upload

---
+++ GP4 Aug 1.py
@@ -0,0 +1,247 @@
+#Purpose: Restructure the previous chatbot as described on Canvas. (Travel Agent Part 2)
+#Author: Anna Fong and Kaitlyn Lum
+#Date: July 28, 2023
+
+# PART 2
+
+import random
+
+#create the lists
+destinations = []
+flight_prices = []
+hotel_prices = []
+highlights = []
+dest_highlights = []
+accumulators = []
+
+#function that reads from the CSV files
+def read_string_from_file(filename, listname):
+    file = open(filename, 'r')
+
+    #loops through CSV file and adds the words into list
+    for aline in file:
+        aline = aline.replace("\n", "")
+        listname.append(aline)
+    #adding to the accumulator list (number of destinations)    
+    if listname == destinations:
+        for i in range(len(destinations)):
+            accumulators.append(0)
+    file.close()
+    return listname
+
+#function writes to user.csv file
+def append_line_to_file(dataString, filename):
+    file = open(filename, 'a')
+ 
+
+    #writes the name or age to the user.csv file
+    if dataString == name or dataString ==age:
+        file.write(str(dataString))
+        file.write(",")
+    if dataString == vacation_activity_booleans:    
+        #writes 0's and 1's to user.csv   
+            #loop writing the 0's and 1's to file
+        for boolean in vacation_activity_booleans:
+            i=0
+        
+                #if it is not the last 0 or 1 to be written in user.csv file
+            if i != len(vacation_activity_booleans):
+                    if boolean == True:
+                        booleans = "1"
+                        file.write(booleans)
+                        file.write(",")
+                        i +=1
+                    elif boolean ==False:
+                        booleans = "0"
+                        file.write(booleans)
+                        file.write(",")
+                        i +=1
+    
+            elif i == len(vacation_activity_booleans):
+
+                if boolean == True:
+                        booleans = "1"
+                        file.write(booleans)
+                        file.write("\n")
+                        i +=1
+                elif boolean == False:
+                        booleans = "0"
+                        file.write(booleans) 
+                        file.write("\n")
+                        i +=1
+                break
+
+
+
+
+
+    file.close()
+
+
+def welcome():
+    print('Welcome! I am your friendly travel agent bot.\n'\
+    'I will ask you some questions, and make a\nrecommendation based on'\
+    ' your answer.\nIf you are interested, I will provide you\n'\
+    'with a one - time password to create a user\naccount on our website.\n')
+
+def ask_user_information ():
+    
+    userName = input("What is your name? --> ")
+    print("Hello dear " + userName + "!\n")
+
+    ask_number("What is your age? --> ")
+
+    userAge = numberquestionresponse
+    global discount
+    discount = compute_discountpercentage(userAge)
+
+    ask_number("\nFor how many nights do you want to stay? --> ")
+    userNights = numberquestionresponse
+
+    return userName, userAge, userNights
+
+vacation_activity_booleans = []
+def ask_user_preferences():
+    
+    for highlight in highlights:
+        highlightQuestion = "Do you like " + highlight + " ?"
+        vacation_activity_booleans.append(ask_yesno(highlightQuestion))
+    return vacation_activity_booleans
+ 
+def ask_yesno(yesnoquestion):
+    i = 0
+    while i == 0:
+        print(yesnoquestion)
+        global yesnoresponse
+        yesnoresponse = input("Please answer y/n. --> ")
+        j = 0
+        while j == 0:
+            if yesnoresponse == "y" or yesnoresponse == "n":
+                break
+            else:
+                print("You didn't type 'y' or 'n'!")
+                newInput = (input("Please answer y/n. --> "))
+                yesnoresponse = newInput
+        break
+
+    if yesnoresponse == "y":
+        yesnoresponse = True
+    elif yesnoresponse == "n":
+        yesnoresponse = False
+    else: 
+        yesnoresponse = False
+
+    return yesnoresponse
+
+def ask_number(numberquestion):
+    i = 0
+    while i == 0:
+        global numberquestionresponse
+        numberquestionresponse = input(numberquestion)
+        j = 0
+        while j == 0:
+            try:
+                numberquestionresponse = int(numberquestionresponse)
+                break
+            except ValueError:
+                print("You didn't type in a (whole) number!")
+                newInput = input("Please type in a number (w/o decimal point) --> ")
+                numberquestionresponse = newInput
+        print("")
+        break
+    return numberquestionresponse
+
+def compute_discountpercentage(userAge):
+    if userAge > 64:
+        seniorDiscount = (userAge - 64)
+        print("Great! We offer a senior discount.")
+        print("For every year over 64, you get 1% off.")
+    else: 
+        seniorDiscount = 0
+    return seniorDiscount
+
+def compute_totalcost(tripDestination, numberOfNights, age):
+    global roundedTotalCost
+    if (tripDestination in destinations):
+        for i in range(len(destinations)):
+            if place == destinations[i]:
+                global index
+                index = i
+        totalCost = (float(flight_prices[index]) + float(hotel_prices[index]) * float(numberOfNights)) * ((100-discount)/100)
+        roundedTotalCost = round(totalCost, 2)
+    else:
+        roundedTotalCost = 0
+    return roundedTotalCost
+
+def show_tripdetails(tripDestination, numberOfNights, age):
+    if roundedTotalCost == 0:
+        print("I'm sorry, we don't have any trips to offer at this point.\n")
+    else:
+
+        print("\nHow about a trip to " + tripDestination + "?")
+        print("Flight: " + str(flight_prices[index]) + "$")
+        print("Hotel: " + str(hotel_prices[index]) + "$/night")
+        print("Discount: " + str(int(discount)) + "%")
+        print("Total for", numberOfNights , "nights (incl. discounts): " + str(roundedTotalCost) + "$\n")
+
+def suggest_trip(booleanValues):
+    file = open("dest_highlights.csv", 'r')
+    for aline in file:
+        aline = aline.replace("\n", "").split(",")
+        dest_highlights.append(aline)
+    for i in range(len(destinations)):
+        for j in (range(len(dest_highlights[i]))):
+            if booleanValues[j] == True and dest_highlights[i][j] == "1":
+                accumulators[i] += 1
+
+    # i = 0 to start off. If new i is larger, then replace i with larger number.
+    count = 0
+    global place
+    for i in range(len(accumulators)):
+        if accumulators[i] > count:
+            count = accumulators[i]
+            place = destinations[i]
+    if count > 0:
+        return place
+    else:
+        place = "No place"
+        return place
+        
+def ask_to_createaccount():
+    ask_yesno("Are you interested, and want to create a user account?")
+
+# If no, display an apology message
+    if yesnoresponse == False:
+        print("Sorry to hear that. Please consider using our service again.")
+        print("\nGoodbye.")
+# If yes, create a password for the user
+    else:
+        lastLetter = name[-1] 
+        beginningPassword = lastLetter * (age % 8)
+        middlePassword = name[0]
+        endPassword = random.randint(0,5) * "!"
+        password = beginningPassword + middlePassword + endPassword
+        print("Looking forward to working with you!\nYour one-time password is: " + password)
+        print("\nGoodbye.")
+
+#call functions
+welcome()
+read_string_from_file("destinations.csv", destinations)
+read_string_from_file("flight_prices.csv", flight_prices)
+read_string_from_file("hotel_prices.csv", hotel_prices)
+read_string_from_file("highlights.csv", highlights)
+
+
+name, age, nights = ask_user_information()
+ask_user_preferences()
+suggest_trip(vacation_activity_booleans)
+compute_totalcost(place, nights, age)
+show_tripdetails(place, nights, age)
+ask_to_createaccount()
+
+
+append_line_to_file(name, "users.csv")
+append_line_to_file(age, "users.csv")
+append_line_to_file(vacation_activity_booleans, "users.csv")
+
+print(vacation_activity_booleans)
